a4eb9efc812c3878996293d5027c618f57f98463
Antek <antek.olszewski01@gmail.com>
2021-01-27 12:50:38 +0100

    nowe dane

--- lmp10/aproksymator_laguerra.c
+++ lmp10/aproksymator_laguerra.c
@@ -6,16 +6,6 @@
 #include <float.h>
 #include <math.h>
 
-#define delta 1.0e-6
-
-int silnia(int n)
-{
-	if (n == 0)
-		return 1;
-	if (n == 1)
-		return 1;
-	return n*silnia(n-1);
-}
 double laguerr(int n, int alfa, double x)
 {
 	if (n==0)
